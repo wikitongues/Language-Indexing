90aabd96d28cd67da7745387f8d29c8ef8e1581f
Vivian Du <vivian_du@berkeley.edu>
2021-05-18 16:33:06 -0700

    Add airtable API parameters page_size and max_records

--- wikitongues/wikitongues/data_store/airtable/airtable_table_info.py
+++ wikitongues/wikitongues/data_store/airtable/airtable_table_info.py
@@ -3,14 +3,18 @@ class AirtableTableInfo:
     Information for retrieving data from a Table on Airtable
     """
 
-    def __init__(self, name, id_column):
+    def __init__(self, name, id_column, page_size=100, max_records=100):
         """
         Construct AirtableTableInfo
 
         Args:
             name (str): Table name
             id_column (str): Name of identifier column
+            page_size (int): Number of records returned in each request
+            max_records (int): Max records to retrieve
         """
 
         self.name = name
         self.id_column = id_column
+        self.page_size = page_size
+        self.max_records = max_records
