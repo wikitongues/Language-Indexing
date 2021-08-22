20223668e5ed41748b53f6b19e1e9ac884552e9a
Scott Rohrer <smrohrer@alumni.cmu.edu>
2021-08-22 13:53:10 -0400

    More refactoring

--- wikitongues/wikitongues/language_indexing.py
+++ wikitongues/wikitongues/language_indexing.py
@@ -3,22 +3,18 @@
 import sys
 import types
 
-from scrapy.crawler import CrawlerProcess
-import importlib
-from inflection import underscore
+from language_indexing_runner import LanguageIndexingRunner
 
 from config.load_configs import \
     load_item_airtable_datastores, load_languages_airtable_datastores
-from spider_input_factory import SpiderInputFactory
 from write_user_config import ask_user_for_user_file_creation
 from language_indexing_config import LanguageIndexingConfiguration, load_config
+
+import config.config_keys as keys
 
 
 def main():
     configs = types.SimpleNamespace()
-
-    # Instantiate configuration object
-    configs.main_config = LanguageIndexingConfiguration()
 
     ask_user_for_user_file_creation()
 
@@ -26,51 +22,9 @@ def main():
     if start.lower() == 'n':
         sys.exit(0)
 
-    load_config(configs.main_config)
-    # The load_config function will try to read user file
-    # as long as there is two arguments passed
-    load_config(configs.main_config, 'user_config')
+    configure(configs)
 
-    # Info required to connect to Airtable
-    configs.item_datastore = load_item_airtable_datastores(configs.main_config)
-    configs.languages_datastore = \
-        load_languages_airtable_datastores(configs.main_config)
-    # load languages table
-    configs.config_languages_table = \
-        configs.main_config['airtable_languages_table']
-    # Configure a CrawlerProcess
-    process = CrawlerProcess(
-        settings={
-            "FEEDS": {
-                "items.jl": {
-                    "format": "jl"
-                }
-            },
-            'ITEM_DATA_STORE': configs.item_datastore,
-            'ITEM_PIPELINES': {
-                'pipelines.WikitonguesPipeline': 300
-            }
-        }
-    )
-
-    def process_site(site):
-        spider_class_name = configs.main_config['spiders'][site]
-        spider_module_name = f'spiders.{underscore(spider_class_name)}'
-
-        spider_class = getattr(
-            importlib.import_module(spider_module_name),
-            spider_class_name)
-
-        spider_input = SpiderInputFactory.get_spider_input(site, configs)
-
-        process.crawl(
-            spider_class,
-            spider_input=spider_input,
-            language_data_store=configs.languages_datastore)
-
-        process.start()
-
-    sites = configs.main_config['sites']
+    sites = configs.main_config[keys.SITES_SECTION]
 
     # start_all_crawls = input('Do you wish to crawl all spiders? (Y/N) ')
 
@@ -85,8 +39,27 @@ def main():
 
     # elif start_all_crawls.lower() == 'y':
     for site in sites.__dict__:
-        process_site(site)
+        LanguageIndexingRunner.process_site(site, configs)
 
     # else:
     #     print('invalid input')
     #     sys.exit(1)
+
+
+def configure(configs):
+    # Instantiate configuration object
+    configs.main_config = LanguageIndexingConfiguration()
+
+    # Read default config
+    load_config(configs.main_config)
+
+    # Read user config
+    load_config(configs.main_config, 'user_config')
+
+    configs.item_datastore = load_item_airtable_datastores(configs.main_config)
+
+    configs.languages_datastore = load_languages_airtable_datastores(
+        configs.main_config)
+
+    configs.config_languages_table = \
+        configs.main_config[keys.AIRTABLE_LANGUAGES_TABLE_SECTION]
